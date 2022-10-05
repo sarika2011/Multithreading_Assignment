43aa77f39f4e11b3d7c261794055c101af214466
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-10-05 07:14:52 +0530

    Add files via upload

---
+++ NewCachedThreadPool.java
@@ -0,0 +1,41 @@
+package Question_2;
+/*2) Use a singleThreadExecutor, newCachedThreadPool() and newFixedThreadPool() to submit a list of tasks and wait for completion of all tasks.*/
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class NewCachedThreadPool {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //Using new cached thread pool
+        ExecutorService ex = Executors.newCachedThreadPool();
+
+        List<Callable<Integer>> callables = new ArrayList<>();
+
+        Callable<Integer> callable = () -> {
+            Thread.sleep(300);
+            return 5;
+        };
+
+        callables.add(callable);
+        callables.add(callable);
+        callables.add(callable);
+        callables.add(callable);
+        callables.add(callable);
+        callables.add(callable);
+
+        List<Future<Integer>> futures = ex.invokeAll(callables);
+        System.out.println(Thread.activeCount());
+        futures.forEach(res -> {
+            try {
+                System.out.println("Result :: " + res.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        ex.shutdown();
+
+    }
+}
